ab6fd67dd5a926b65a97b25f4f7edf69e1e477f8
unknown <rerheza@gmail.com>
2021-09-26 18:34:50 +0700

    solusi tak hingga dan unik gaussjordan, blm terlalu debugging

--- src/gaussjordan.java
+++ src/gaussjordan.java
@@ -5,7 +5,19 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class MyClass {
-
+	
+	// ngetes suatu string apakah numerik
+	static boolean tesDouble(String x) {
+		
+		try {
+		    double doubleValue = Double.parseDouble(x);
+		    return true;
+		} catch (NumberFormatException e) {
+		    return false;
+		}
+	}
+	
+	// fungsi buat nuker baris
     static void swap_row(double mat[][], int col, int i, int j){
         for (int k = 0; k < col; k++){
             double temp = mat[i][k];
@@ -16,8 +28,8 @@ public class MyClass {
 	
 	static void gaussJordan(double mat[][], int row, int col){
 		
-		// Proses gauss
-		int idxCol = 0;
+		// Proses gauss biasa
+		int idxCol = 0; // Sekarang lagi mau ngebuat angak 1 di kolom ke berapa 
 		for (int i = 0; i < row; i++) {
 			
 			// Ngecek udah sampe kolom terakhir belom
@@ -25,12 +37,12 @@ public class MyClass {
 				break;
 			}
 			
-			// Ngecek depannya udah bukan 0
+			// Ngecek apakah depannya udah bukan 0
 			boolean cek;
 			while (true) {
 				if (mat[i][idxCol] == 0) {
 					cek = false;
-					for (int j = i + 1; j < row; j++) {
+					for (int j = i + 1; j < row; j++) { // cari sampe ketemu yang bukan nol di kolom
 						if (mat[j][idxCol] != 0) {
 							swap_row(mat, col, i, j);
 							cek = true;
@@ -39,18 +51,19 @@ public class MyClass {
 					}
 					if (cek) {
 						break;
-					} else {
+					} else { // di idxCol ga ada angka selain 0, pindah kolom lain
 						idxCol++;
 					}
-				} else {
+				} else { // depannya ga 0
 					break;
 				}
+				// ngecek apakah idxCol dah pindah sampe ujung, utk ngebreak while
 				if (idxCol == col - 1) {
 					break;
 				}
 			}
 
-			// Ngecek udah sampe kolom terakhir belom
+			// Ngecek udah sampe kolom terakhir belom, ini utk ngebreak for ny
 			if (idxCol == col - 1) {
 				break;
 			}			
@@ -82,28 +95,27 @@ public class MyClass {
 		// proses backtracking
 		
 		// Ngurangin semua yang di atas 1 biar jadi 0
-		int idxSatu;
+		int idxSatu; //untuk indeks satu terbawah tiap kolom
 		
 		for (int i = col - 2; i > -1; i--) {
-			
-			// Nyari letak 1 di col ini
+			// Nyari letak 1 terbawah di col ini
 			idxSatu = row - 1;
 			while (true) {
 				if (mat[idxSatu][i] == 1) {
 					break;
 				}
 				idxSatu--;
-				if (idxSatu == -1) {
+				if (idxSatu == -1) { // ga ada 1 di col, break while
 					break;
 				}
 			}
 			
-			// Berarti ga ada 1 di col itu
+			// Berarti ga ada 1 di col itu, pindah kolom
 			if (idxSatu == -1) {
 				continue;
 			}
 			
-			// Kurangin elemen non 0 terbelakang biar jadi 0
+			// Kurangin elemen non 0 di atas 1 biar jadi 0
 			double pengali;
 			for (int j = idxSatu - 1; j > -1; j--) {
 				if (mat[j][i] == 0) {
@@ -115,6 +127,74 @@ public class MyClass {
 				}
 			}
 		}
+		
+		// cek jenis solusi
+		boolean cekNolKiri = true; // cek apakah row matriks kiri terbawah nol semua
+		boolean cekNolKanan = true; // cek apakah row matriks kanan terbawah nol
+		for (int i = 0; i < col-1; i++) {
+			if (mat[row-1][i] != 0) {
+				cekNolKiri = false;
+			}
+		}
+		if (mat[row-1][col-1] != 0) {
+			cekNolKiri = false;
+		}
+		
+		if (cekNolKiri && cekNolKanan) {
+			System.out.println("Solusi ada tak hingga");
+		} else if (cekNolKiri) {
+			System.out.println("Tidak ada solusi");
+		} else {
+			System.out.println("Solusi unik");
+		}
+		
+		// List nama variabel hasil yang mungkin (apabila solusi tak hingga)
+		String listVar[] = new String[]{"a", "b", "c", "d","e","f","g","h","i","j","k"};
+		int cnt = 0; //nama var apa yang udh kepake
+		
+		// inisialisasi array jawaban
+		String ans[] = new String[col-1];
+		for (int i = 0; i < col-1; i++) {
+			ans[i] = "tai";
+		}
+		
+		// menghitung hasil dan meletakkannya dalam matriks jawaban
+		for (int i = row-1; i > -1; i--) {
+			for (int j = col-2; j > -1; j--) {
+				
+				if ((mat[i][j]==1) && (ans[j]=="tai")) {
+					
+					double yangNilai = mat[i][col-1];
+					String yangVar = "";
+					for (int k = j + 1; k < col-1; k++) {
+						if (tesDouble(ans[k])) {
+							yangNilai = yangNilai - mat[i][k] * Double.parseDouble(ans[k]);
+						} else {
+							if (mat[i][k] == 0) {
+								continue;
+							} else if (mat[i][k] == 1) {
+								yangVar = yangVar + " - " + ans[k];
+							} else if (mat[i][k] == -1) {
+								yangVar = yangVar + " + " + ans[k];
+							} else if (mat[i][k] < 0){
+								yangVar = yangVar + " " + String.format("%f", mat[i][k]) + ans[k];
+							} else {
+								yangVar = yangVar + " - " +String.format("%f", mat[i][k]) + ans[k];
+							}
+						}
+					}
+
+					ans[j] = String.format("%f", yangNilai) + yangVar;
+					break;
+						
+				} else if (ans[j] == "tai") {
+					ans[j] = listVar[cnt];
+					cnt++;
+				}
+			}
+		}
+		
+		System.out.println(Arrays.deepToString(ans));
 	}
 	
 	// Driver
